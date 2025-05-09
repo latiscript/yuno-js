625f5dbefabc1acbe02631b789413c6601f2caa5
Miguel Velasquez <miguel.velasquez@y.uno>
2025-05-09 13:36:00 -0500

    add gitignore and improve mcps

--- packages/yuno-js-sdk-mcp/src/index.ts
+++ packages/yuno-js-sdk-mcp/src/index.ts
@@ -6,37 +6,47 @@ const server = new McpServer({
   version: "1.0.0",
 });
 
-server.tool("yuno-sdk-knowledgebase-retrieve", "Retrieve information from github readme file", async () => {
-  const response = await fetch("https://raw.githubusercontent.com/yuno-payments/yuno-sdk-web/refs/heads/main/README.md");
-  const text = await response.text();
-  return {
-    content: [{ type: "text", text }],
-  };
+server.tool("yuno-sdk-retrieve-readme", "Retrieve information from github readme file", async () => {
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/yuno-payments/yuno-sdk-web/refs/heads/main/README.md");
+    const text = await response.text();
+    return {
+      content: [{ type: "text", text }],
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        content: [{ type: "text", text: error.message }],
+      };
+    }
+    return {
+      content: [{ type: "text", text: "An unknown error occurred" }],
+    };
+  }
 });
 
-server.tool("yuno-sdk-knowledgebase-retrieve", "Retrieve information from github react example", async () => {
-  const fullExample = await fetch(
-    "https://raw.githubusercontent.com/latiscript/yuno-js/refs/heads/main/examples/react-express/react/src/components/Full.tsx"
-  );
-
-  const fullImplementation = await fetch(
-    "https://raw.githubusercontent.com/latiscript/yuno-js/refs/heads/main/packages/yuno-react/src/components/full/Full.tsx"
-  );
-
-  const fullImplementationTypes = await fetch(
-    "https://raw.githubusercontent.com/latiscript/yuno-js/refs/heads/main/packages/yuno-react/src/components/full/types.ts"
-  );
+server.tool("yuno-sdk-retrieve-react-example", "Retrieve information from github react example", async () => {
+  try {
+    const fullExample = await fetch(
+      "https://raw.githubusercontent.com/latiscript/yuno-js/refs/heads/main/examples/react-express/react/src/components/Full.tsx"
+    );
 
   const fullExampleText = await fullExample.text();
-  const fullImplementationText = await fullImplementation.text();
-  const fullImplementationTypesText = await fullImplementationTypes.text();
   return {
     content: [
       { type: "text", text: fullExampleText },
-      { type: "text", text: fullImplementationText },
-      { type: "text", text: fullImplementationTypesText },
     ],
-  };
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        content: [{ type: "text", text: error.message }],
+      };
+    }
+    return {
+      content: [{ type: "text", text: "An unknown error occurred" }],
+    };
+  }
 });
 const transport = new StdioServerTransport();
 await server.connect(transport);
